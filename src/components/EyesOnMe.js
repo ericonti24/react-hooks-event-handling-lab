64d2a09000d1aaf995165fc284e4784b5385711b
Eric <ericontiveros123@gmail.com>
2022-05-19 15:24:44 -0700

    commit tree c2f24c1df54c661d26a2429d93ee6b7d09caa967

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+function EyesOnMe(){
+
+    return(
+        <divv>
+            <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </divv>
+    )
+}
+
+export default EyesOnMe
